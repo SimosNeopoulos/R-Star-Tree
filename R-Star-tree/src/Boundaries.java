919dcd0ea44f44bd850840d2797314afed6347a1
SimosNeopoulos <simneo22@gmail.com>
2023-04-02 12:36:09 +0300

    Added Boundaries class

---
+++ R-Star-tree/src/Boundaries.java
@@ -0,0 +1,18 @@
+public class Boundaries {
+
+    private double upperBound;
+    private double lowerBound;
+
+    public Boundaries(double upperBound, double lowerBound) {
+        this.upperBound = upperBound;
+        this.lowerBound = lowerBound;
+    }
+
+    public double getUpperBound() {
+        return upperBound;
+    }
+
+    public double getLowerBound() {
+        return lowerBound;
+    }
+}
