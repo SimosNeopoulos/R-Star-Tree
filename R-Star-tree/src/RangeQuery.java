b7cb809815707ce494fd85376209d25e46060d08
SimosNeopoulos <simneo22@gmail.com>
2023-05-02 21:19:15 +0300

    Range Query

---
+++ R-Star-tree/src/RangeQuery.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class RangeQuery {
+    private ArrayList<LeafEntry> validEntries;
+    private Node root;
+    private BoundingRectangle searchArea;
+
+    public RangeQuery(Node root, BoundingRectangle searchArea) {
+        this.root = root;
+        this.searchArea = searchArea;
+    }
+
+    public ArrayList<LeafEntry> rangeQuerySearch() {
+        validEntries = new ArrayList<>();
+        search(root, searchArea);
+        return validEntries;
+    }
+
+    public ArrayList<LeafEntry> rangeQuerySearch(BoundingRectangle searchArea) {
+        validEntries = new ArrayList<>();
+        this.searchArea = searchArea;
+        search(root, this.searchArea);
+        return validEntries;
+    }
+
+    private void search(Node node, BoundingRectangle searchBox) {
+        if (!node.isLeaf()) {
+
+            for (Entry entry : node.getEntries()) {
+                NonLeafEntry nonLeafEntry = (NonLeafEntry) entry;
+
+                if (EntriesCalculator.checkOverlap(entry.getBoundingRectangle(), searchBox)) {
+                    search(DataHandler.getNodeFromIndexFile(nonLeafEntry.getChildPTR()), searchBox);
+                }
+            }
+
+        } else {
+
+            for (Entry entry : node.getEntries()) {
+
+                LeafEntry leafEntry = (LeafEntry) entry;
+                if (EntriesCalculator.checkOverlap(leafEntry.getBoundingRectangle(), searchBox)) {
+                    validEntries.add(leafEntry);
+                }
+            }
+        }
+    }
+
+
+}
